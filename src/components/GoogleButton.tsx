12e8c8742586e12a0fb8a89bc70e5a76cc1f5254
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-10-05 01:01:03 +0300

    Update GoogleButton.tsx

--- src/components/GoogleButton.tsx
+++ src/components/GoogleButton.tsx
@@ -4,11 +4,11 @@ import { useSearchParams } from "next/navigation";
 
 const GoogleButton = () => {
   const searchParams = useSearchParams();
-  // const callbackUrl = searchParams.get("callbackUrl") || "/profile";
+  const callbackUrl = searchParams.get("callbackUrl") || "/profile";
   return (
     <button
       onClick={() => {
-        signIn("google");
+        signIn("google", { callbackUrl });
       }}
     >
       Sign in with Google
